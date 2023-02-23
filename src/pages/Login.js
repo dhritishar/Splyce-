efa9ecd3660caad3cbfbf1d8323036a66677a83e
dhritishar <109621401+dhritishar@users.noreply.github.com>
2023-02-23 18:22:42 -0500

    find

---
+++ src/pages/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form, Input, message } from 'antd';
+import "../styles/RegisterStyles.css";
+import { Link, useNavigate} from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+    const navigate = useNavigate();
+    //form handler
+    const onfinishHandler = async (values) => {
+      try {
+        const res = await axios.post("/api/v1/users/login", values);
+        if (res.data.success) {
+          localStorage.setItem("token", res.data.token);
+          message.success("Login Successful");
+          navigate("/");
+        } else {
+          message.error(res.data.message);
+        }
+      } catch (error) {
+        console.log(error);
+        message.error("something went wrong");
+      }
+    };
+  return (
+    <div className="form-container">
+        <Form layout="vertical" onFinish={onfinishHandler} className="login-form">
+            <h3 className="text-center">Login</h3>
+            
+            <Form.Item label="Email" name="email">
+                <Input type="email" required />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+                <Input type="password" required />
+            </Form.Item>
+            <Link to="/register" className="m-3">Not a user? Register here</Link>
+            <button className="btn btn-primary" type="submit">Login</button>
+
+        </Form>
+      
+    </div>
+  )
+}
+
+export default Login
